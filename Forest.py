fb490e214f2dd82e4358c537b0bb798d6e81cc56
Thomas Waters <twaters117@gmail.com>
2018-10-24 20:11:13 -0400

    Renamed forest to matrix

--- Forest.py
+++ Forest.py
@@ -5,27 +5,27 @@ class Forest:
   def __init__(self, size):
     self.num_trees = 0
     self.size = size
-    self.forest = np.zeros((self.size, self.size), dtype=int)
+    self.matrix = np.zeros((self.size, self.size), dtype=int)
 
   def copy(self):
     other = Forest(self.size)
     other.num_trees = self.num_trees
     other.size = self.size
-    other.forest = np.copy(self.forest)
+    other.matrix = np.copy(self.matrix)
 
     return other
 
   def getValue(self, location):
-    return self.forest.item(location)
+    return self.matrix.item(location)
 
   def getNumTrees(self):
     return self.num_trees
 
   def setValue(self, location, value):
-    self.forest.itemset(location, value)
+    self.matrix.itemset(location, value)
 
   def getForestMatrix(self):
-    return self.forest
+    return self.matrix
 
   def getNeighborLocations(self, location):
     x = location[0]
@@ -81,7 +81,7 @@ class Forest:
   def getUnoccupiedLocations(self):
     unnocupied_locations = []
 
-    for index, forest_size in np.ndenumerate(self.forest):
+    for index, forest_size in np.ndenumerate(self.matrix):
       if forest_size == 0:
         unnocupied_locations.append(index)
 
